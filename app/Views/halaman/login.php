4b48fd8c554fd558c7a17b577c955f4993688b7c
Yesterdayland <asder230498@gmail.com>
2021-11-04 14:19:44 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Perpustakan Login</title>
+    </head>
+    <body>
+        
+        <h1>Selamat datang di sistem informasi perpustakaan</h1>
+        <p>
+            Ini adalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url('/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member
+        </p>
+    </body>
+</html>
